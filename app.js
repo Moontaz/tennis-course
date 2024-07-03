aaddc513bde3c3c46379e933e467f687560043d4
Moontaz <riedyriedy283@gmail.com>
2024-07-03 12:00:26 +0700

    little changes

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-// const observer = new IntersectionObserver((entries, observer) => {
-//   entries.forEach((entry) => {
-//     console.log(entry);
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add("show");
-//       observer.unobserve(entry.target); // Unobserve the element after adding the class
-//     }
-//   });
-// });
-
-// const hiddenElements = document.querySelectorAll(".hidden");
-// hiddenElements.forEach((el) => observer.observe(el));
-
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
     console.log(entry);
     if (entry.isIntersecting) {
-      entry.target.classList.remove("hidden");
       entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
+      observer.unobserve(entry.target); // Unobserve the element after adding the class
     }
   });
 });
+
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       entry.target.classList.remove("hidden");
+//       entry.target.classList.add("show");
+//     } else {
+//       entry.target.classList.remove("show");
+//     }
+//   });
+// });
+// const hiddenElements = document.querySelectorAll(".hidden");
+// hiddenElements.forEach((el) => observer.observe(el));
